d1aee88b96b9733137e3ab7add19af37d533bf67
wuhongsen <1535430014@qq.com>
2021-06-29 22:21:53 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/whs/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.whs.git;
+
+
+
+/**
+ * @author wuhongsen
+ * @creat 2021-06-29-22:09
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
